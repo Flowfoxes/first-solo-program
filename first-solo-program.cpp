e4a1a4993e4b11f8727d03bdb8908155713f7a0e
Rohr Raney <rohrraney@gmail.com>
2023-10-11 18:49:29 +0200

    trying

--- first-solo-program.cpp
+++ first-solo-program.cpp
@@ -7,9 +7,35 @@
 
 int main()
 {
-   
+    initialization();
+
+    return 0;
 }
 
+void initialization()
+{
+
+    std::cout << "Welcome to your all in one personal Smart Home appliance manager.\n";
+    std::cout << "Please enter your login in credentials below.\n" << std::endl;
+    std::cout << "login/username\n" << std::endl;
+    std::cin login;
+
+    mainMenu();
+
+}
+
+void mainMenu()
+{
+    using std::cout;
+    cout << "Main menu of available functions:\n";
+    cout << "1) Simple addition\n";
+    cout << "2) Simple subtraction\n";
+    cout << "3) Simple multiplication\n";
+    cout << "4) Simple division\n";
+    cout << "+) Advanced functions\n";
+    cout << "Press \"E\" for exit\n";
+
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
